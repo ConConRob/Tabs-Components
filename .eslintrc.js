350af75338d60edcfe9c48d14ce692fc6edd8e7f
conconrob <connortprobertson@gmail.com>
2019-01-31 12:13:57 +0000

    CONE dropdown menu

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "rules": {
+        "linebreak-style": 0,
+        "quote-props": 0,
+        "quotes": 0,
+        "no-plusplus":0,
+        "no-param-reassign":0,
+    },
+};
